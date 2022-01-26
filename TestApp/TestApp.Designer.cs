7ea78b72d07733d0b456868c37e58c3add46bb89
RikOShey <84726357+RikOShey@users.noreply.github.com>
2022-01-26 15:07:58 +0100

    Add files via upload

---
+++ TestApp/TestApp.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace TestApp
+{
+    partial class TestApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TestApp));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Player1 = new System.Windows.Forms.TextBox();
+            this.Player2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(445, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Spelare 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(445, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Spelare 2:";
+            // 
+            // Player1
+            // 
+            this.Player1.Location = new System.Drawing.Point(528, 18);
+            this.Player1.Name = "Player1";
+            this.Player1.Size = new System.Drawing.Size(127, 22);
+            this.Player1.TabIndex = 11;
+            // 
+            // Player2
+            // 
+            this.Player2.Location = new System.Drawing.Point(528, 47);
+            this.Player2.Name = "Player2";
+            this.Player2.Size = new System.Drawing.Size(127, 22);
+            this.Player2.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(447, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(210, 23);
+            this.label3.TabIndex = 13;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(450, 355);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(205, 38);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Starta";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(453, 122);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // TestApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.ClientSize = new System.Drawing.Size(674, 410);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Player2);
+            this.Controls.Add(this.Player1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "TestApp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Luffarschack";
+            this.Load += new System.EventHandler(this.TestApp_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Player1;
+        private System.Windows.Forms.TextBox Player2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ TestApp/TestApp.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestApp
+{
+    public partial class TestApp : Form
+    {
+        private List<PictureBox> boxes = new List<PictureBox>();
+        string player;
+
+        public TestApp()
+        {
+            InitializeComponent();
+        }
+
+        private void TestApp_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                PictureBox newPB = new PictureBox();
+                newPB.Name = "PicBox" + i.ToString();
+                newPB.SizeMode = PictureBoxSizeMode.StretchImage;
+                newPB.Image = null;
+                newPB.Size = new Size(100, 100);
+                newPB.BorderStyle = BorderStyle.Fixed3D;
+                newPB.Enabled = false;
+                newPB.Click += new EventHandler(pb_Click);
+                newPB.Cursor = Cursors.Hand;
+
+                if (i == 0) newPB.Location = new Point(12, 12);
+                if (i == 1) newPB.Location = new Point(117, 12);
+                if (i == 2) newPB.Location = new Point(222, 12);
+                if (i == 3) newPB.Location = new Point(12, 117);
+                if (i == 4) newPB.Location = new Point(117, 117);
+                if (i == 5) newPB.Location = new Point(222, 117);
+                if (i == 6) newPB.Location = new Point(12, 222);
+                if (i == 7) newPB.Location = new Point(117, 222);
+                if (i == 8) newPB.Location = new Point(222, 222);
+
+                boxes.Add(newPB);
+                this.Controls.Add(newPB);
+            }
+            pictureBox1.Location = new Point(339, 100);
+            pictureBox1.Size = new Size(150, 150);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+
+        void pb_Click(object sender, EventArgs e)
+        {
+            PictureBox clickedPictureBox = sender as PictureBox;
+            if (player == Player1.Text)
+            {
+                clickedPictureBox.Image = Properties.Resources.GreenButton;
+                clickedPictureBox.Tag = 1;
+                clickedPictureBox.Enabled = false;
+                CheckBoxes();
+                player = Player2.Text;
+                label3.Text = "Spelare: " + player;
+            }
+            else
+            {
+                clickedPictureBox.Image = Properties.Resources.RedButton;
+                clickedPictureBox.Tag = 2;
+                clickedPictureBox.Enabled = false;
+                CheckBoxes();
+                player = Player1.Text;
+                label3.Text = "Spelare: " + player;
+            }
+        }
+
+        private void CheckBoxes()
+        {
+            if (boxes[0].Tag != null && boxes[1].Tag != null && boxes[2].Tag != null)
+            {
+                if (boxes[0].Tag.ToString() == boxes[1].Tag.ToString() && boxes[1].Tag.ToString() == boxes[2].Tag.ToString())
+                {
+                    PlayAgain("win");
+                }
+            }
+
+            if (boxes[3].Tag != null && boxes[4].Tag != null && boxes[5].Tag != null)
+            {
+                if (boxes[3].Tag.ToString() == boxes[4].Tag.ToString() && boxes[4].Tag.ToString() == boxes[5].Tag.ToString())
+                {
+                    PlayAgain("win");
+                }
+            }
+
+            if (boxes[6].Tag != null && boxes[7].Tag != null && boxes[8].Tag != null)
+            {
+                if (boxes[6].Tag.ToString() == boxes[7].Tag.ToString() && boxes[7].Tag.ToString() == boxes[8].Tag.ToString())
+                {
+                    PlayAgain("win");
+                }
+            }
+
+            if (boxes[0].Tag != null && boxes[3].Tag != null && boxes[6].Tag != null)
+            {
+                if (boxes[0].Tag.ToString() == boxes[3].Tag.ToString() && boxes[3].Tag.ToString() == boxes[6].Tag.ToString())
+                {
+                    PlayAgain("win");
+                }
+            }
+
+            if (boxes[1].Tag != null && boxes[4].Tag != null && boxes[7].Tag != null)
+            {
+                if (boxes[1].Tag.ToString() == boxes[4].Tag.ToString() && boxes[4].Tag.ToString() == boxes[7].Tag.ToString())
+                {
+                    PlayAgain("win");
+                }
+            }
+
+            if (boxes[2].Tag != null && boxes[5].Tag != null && boxes[8].Tag != null)
+            {
+                if (boxes[2].Tag.ToString() == boxes[5].Tag.ToString() && boxes[5].Tag.ToString() == boxes[8].Tag.ToString())
+                {
+                    PlayAgain("win");
+                }
+            }
+
+            if (boxes[0].Tag != null && boxes[4].Tag != null && boxes[8].Tag != null)
+            {
+                if (boxes[0].Tag.ToString() == boxes[4].Tag.ToString() && boxes[4].Tag.ToString() == boxes[8].Tag.ToString())
+                {
+                    PlayAgain("win");
+                }
+            }
+
+            if (boxes[2].Tag != null && boxes[4].Tag != null && boxes[6].Tag != null)
+            {
+                if (boxes[2].Tag.ToString() == boxes[4].Tag.ToString() && boxes[4].Tag.ToString() == boxes[6].Tag.ToString())
+                {
+                    PlayAgain("win");
+                }
+            }
+            
+            if (boxes[0].Tag != null && boxes[1].Tag != null && boxes[2].Tag != null && boxes[3].Tag != null && boxes[4].Tag != null && boxes[5].Tag != null && boxes[6].Tag != null && boxes[7].Tag != null && boxes[8].Tag != null)
+            {
+                PlayAgain("tie");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(Player1.Text == "" || Player2.Text == "")
+            {
+                MessageBox.Show ("Man måste ange namn i båda fälten!", "Ange namn...", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                player = Player1.Text;
+                button1.Enabled = false;
+            }
+
+            foreach (Control child in this.Controls)
+            {
+                if (child is PictureBox)
+                {
+                    ((PictureBox)child).Image = null;
+                    ((PictureBox)child).Tag = null;
+                    ((PictureBox)child).Enabled = true;
+                }
+            }
+            label3.Text = "Spelare: " + player;
+        }
+
+        private void PlayAgain(string result)
+        {
+            if (result == "win")
+            {
+                label3.Text = "Vinnare: " + player;
+                pictureBox1.Image = Properties.Resources.Winner;
+                DialogResult dialogResult = MessageBox.Show("Vinnare: " + player + "\nVill ni spela igen?", "Fråga...", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (dialogResult == DialogResult.Yes)
+                {
+                    button1_Click(this, null);
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    Environment.Exit(0);
+                }
+            }
+            else
+            {
+                label3.Text = "Oavgjort!";
+                pictureBox1.Image = Properties.Resources.TiedGame;
+                DialogResult dialogResult = MessageBox.Show("Oavgjort!\nVill ni spela igen?", "Fråga...", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+                if (dialogResult == DialogResult.Yes)
+                {
+                    button1_Click(this, null);
+                }
+                else if (dialogResult == DialogResult.No)
+                {
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
